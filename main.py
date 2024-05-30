165780e114ff3c8268b7e1d5ca08d77e0b076b44
Sebastian Clancy <foldit.com@gmail.com>
2024-05-30 16:05:41 -0400

    Changed output.

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import subprocess
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
-    subprocess.run(["node", "--version"])
-    return 'Hello, World!'
+    result = subprocess.run(["node", "--version"], capture_output=True)
+    string_result = str(result.stdout)
+    return 'Hello, World!' + "<br \\>" + string_result
+
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=8000, debug=False)
