dc9e9cfcee78947d4fe42f1397eb70b01d64f5a3
Sebastian Clancy <foldit.com@gmail.com>
2024-05-30 15:51:25 -0400

    Add node version to main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask
+import subprocess
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    subprocess.run(["node", "--version"])
     return 'Hello, World!'
 
 if __name__ == '__main__':
