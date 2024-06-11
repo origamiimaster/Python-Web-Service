b5662c2ce335eacabccf4bde5e5b03726cca1d54
Sebastian Clancy <foldit.com@gmail.com>
2024-06-11 17:31:12 +0000

    Changed main to use static files

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-from flask import Flask
+from flask import Flask, redirect
 import subprocess
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', static_folder='static')
+
 
 
 @app.route('/')
 def hello_world():
-    result = subprocess.run(["node", "--version"], capture_output=True)
-    string_result = str(result.stdout)
-    return 'Hello, World!' + "<br \\>" + string_result
-
+    # result = subprocess.run(["node", "--version"], capture_output=True)
+    # string_result = str(result.stdout)
+    # return 'Hello, World!' + "<br \\>" + string_result
+    return redirect("/index.html", 302)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000, debug=False)
